929765b3b37cf76241ed3c48c26ebab88dff8d8e
lucrampro <lucienramassamy@gmail.com>
2018-06-28 11:52:00 +0200

    test du js

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+TweenMax.from(".bleu", 1, {width:0, onComplete:function(){
+    
+    TweenMax.from(".bleu p", 0.5,{left:"-100%"})
+    TweenMax.from(".bleu p", 0.5,{visibility: visible;})
+    
+}});
