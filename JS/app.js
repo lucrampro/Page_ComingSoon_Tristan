8b9f554fddd23b505af829ffaef66adc473b54a0
lucrampro <lucienramassamy@gmail.com>
2018-06-28 12:58:49 +0200

    supression du contenu pour bien visualiser l'animation que je voudrais

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,15 @@
-TweenMax.from(".bleu", 1, {width:0, onComplete:function(){
+TweenMax.from(".bleu", 0.5, {width:0,ease: Bounce.easeOut, onComplete: function(){
     
-    TweenMax.from(".bleu p", 0.5,{left:"-100%"})
-    TweenMax.from(".bleu p", 0.5,{visibility: visible;})
     
-}});
+    TweenMax.to(".J1",0.5, { width:"20%" });
+    TweenMax.to(".J2",0.5, { width:"20%", delay:0.2, function(){
+        
+        TweenMax.to(".J1",0.5, { height:"0%", delay:0.5 });
+        TweenMax.to(".J2",0.5, { height:"0%", delay: 0.8 });
+        
+        
+    } });
+    
+    
+} });
+
